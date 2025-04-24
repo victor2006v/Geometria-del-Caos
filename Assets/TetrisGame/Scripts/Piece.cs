ef1d559ee71f025bd7580250913bab51e0e32631
victor2006v <a22viccarnav@inspedralbes.cat>
2025-04-24 19:56:21 +0200

    The controls are perfect, we have rotation with Q and E depends on the direction 1 or -1, space to block the piece and s move down, a and d to move horizontally

--- Assets/TetrisGame/Scripts/Piece.cs
+++ Assets/TetrisGame/Scripts/Piece.cs
@@ -7,11 +7,13 @@ public class Piece : MonoBehaviour
     public PiecesData data { get; private set; }
     public Vector3Int[] cells { get; private set; }
     public Vector3Int position { get; private set; }
+    public int rotationIndex { get; private set; }
 
     public void Initialize(Board board, Vector3Int position, PiecesData data) { 
         this.board = board;
         this.position = position;
         this.data = data;
+        this.rotationIndex = 0;
         if (this.cells == null) { 
             this.cells = new Vector3Int[data.cells.Length];
         }
@@ -19,17 +21,35 @@ public class Piece : MonoBehaviour
             this.cells[i] = (Vector3Int)data.cells[i];
         }
     }
+    /*CONTROLS*/
     private void Update()
     {
         this.board.Clear(this);
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            Move(Vector2Int.left);
-        }   else if (Input.GetKeyDown(KeyCode.D)){
-            Move(Vector2Int.right);
+        /*ROTATION*/
+        if (Input.GetKeyDown(KeyCode.Q)) {
+            Rotate(-1);
+        } else if(Input.GetKeyDown(KeyCode.E)){
+            Rotate(1);
         }
+        /*MOVEMENT*/
+        else if (Input.GetKeyDown(KeyCode.A)) {
+            Move(Vector2Int.left);
+        } else if (Input.GetKeyDown(KeyCode.D)) {
+            Move(Vector2Int.right);
+        } else if (Input.GetKeyDown(KeyCode.S)) {
+            Move(Vector2Int.down);
+        } else if (Input.GetKeyDown(KeyCode.Space)) {
+            HardDrop();
+        }
+
         this.board.Set(this);
 
+    }
+    /*To block the piece*/
+    private void HardDrop() {
+        while (Move(Vector2Int.down)) {
+            continue;
+        }
     }
     /*It moves the piece but before it checks if the position is valid*/
     private bool Move(Vector2Int translation)
@@ -44,6 +64,38 @@ public class Piece : MonoBehaviour
 
         }
         return valid;
+    }
+    private void Rotate(int direction) {
+        this.rotationIndex = Wrap(this.rotationIndex + direction, 0, 4);
+        for (int i = 0; i < this.cells.Length; i++){
+            Vector3 cell = this.cells[i];
+
+            int x, y;
+
+            switch (this.data.piece) {
+                case Pieces.I:
+                case Pieces.O:
+                    cell.x -= 0.5f;
+                    cell.y -= 0.5f;
+                    x = Mathf.CeilToInt((cell.x * Data.RotationMatrix[0] * direction) + (cell.y * Data.RotationMatrix[1] * direction));
+                    y = Mathf.CeilToInt((cell.x * Data.RotationMatrix[2] * direction) + (cell.y * Data.RotationMatrix[3] * direction));
+                    break;
+                default:
+                    x = Mathf.RoundToInt((cell.x * Data.RotationMatrix[0] * direction) + (cell.y * Data.RotationMatrix[1] * direction));
+                    y = Mathf.RoundToInt((cell.x * Data.RotationMatrix[2] * direction) + (cell.y * Data.RotationMatrix[3] * direction));
+                    break;       
+            }
+            this.cells[i] = new Vector3Int(x, y, 0);
+        }
+    }
+    private int Wrap(int input, int min, int max) {
+        if (input < min)
+        {
+            return max - (min - input) % (max - min);
+        }
+        else { 
+            return min + (input - min) % (max - min);
+        }
     }
     
 }
